097c469a58c75add2121a97d1386ce4515cc7aa7
Yungchih Chen <yungchih_chen@yahoo.com>
2019-02-13 17:49:03 -0500

    Add getDog

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,17 @@ import React from 'react';
 import Button from '../containers/Button';
 import NewsItem from '../containers/NewsItem'
 import Loading from '../containers/Loading'
+import Dog from '../containers/Dog'
+import DogItem from '../containers/DogItem'
 
 
 let App = () => (
   <div>
     <Button />
+    <Dog/>
     <Loading />
     <NewsItem />
+    <DogItem/>
   </div>
 );
 
